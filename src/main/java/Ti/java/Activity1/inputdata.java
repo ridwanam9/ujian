304d8a4f67e6cad4f4d7802805eb63e5ca5bfacc
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2021-12-21 07:56:07 +0700

    first commit

---
+++ src/main/java/Ti/java/Activity1/inputdata.java
@@ -0,0 +1,42 @@
+package Ti.java.Activity1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package java.ti.Activity1;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author DELL
+ */
+//@Controller
+//public class inputdata {
+//    
+//    String nama_barang;
+//    String harga_kilo;
+//    String jumlah_beli;
+//    
+//    @RequestMapping("/activity1")
+////    @ResponseBody
+//    public void lihatdata(HttpServletRequest data, Model model){
+//    
+//        nama_barang = data.getParameter("name");
+//        harga_kilo = data.getParameter("cost");
+//        jumlah_beli = data.getParameter("amount");
+//        
+////        int product_cost = early_cost(harga_kilo, jumlah_beli);
+////        
+////        model.addAttribute("vara", nama_barang);
+////        model.addAttribute("varb", harga_kilo);
+////        model.addAttribute("jumlah", jumlah_beli);
+//        
+//    }
+//    
+//}
